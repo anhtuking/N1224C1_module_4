655d66374fcb579a5b286d40791dce1fe657947d
Anh Tu <anhtu@example.comple.com>
2025-03-05 20:23:52 +0700

    update exercise

--- src/main/java/com/techzen/academy_n1224c1/repository/IEmployeeRepository.java
+++ src/main/java/com/techzen/academy_n1224c1/repository/IEmployeeRepository.java
@@ -6,28 +6,29 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query(value = """
-            SELECT e.* FROM employee e
-            LEFT JOIN department d ON e.id_department_id = d.id
-            WHERE (:name IS NULL OR e.name LIKE CONCAT('%', :name, '%'))
-            AND (:dobFrom IS NULL OR e.birth >= :dobFrom)
-            AND (:dobTo IS NULL OR e.birth <= :dobTo)
-            AND (:gender IS NULL OR e.gender = :gender)
-            AND (:phone IS NULL OR e.phone LIKE CONCAT('%', :phone, '%'))
-            AND (:departmentId IS NULL OR d.id = :departmentId)
-            AND (
-                CASE
-                    WHEN :salaryRange = 'lt5' THEN e.salary < 5000000
-                    WHEN :salaryRange = '5-10' THEN e.salary BETWEEN 5000000 AND 10000000
-                    WHEN :salaryRange = '10-20' THEN e.salary BETWEEN 10000000 AND 20000000
-                    WHEN :salaryRange = 'gt20' THEN e.salary > 20000000
-                    ELSE TRUE
-                END
-            )
-            """, nativeQuery = true)
+        SELECT e.* FROM employee e
+        LEFT JOIN department d ON e.department_id = d.id
+        WHERE (:#{#searchRequest.name} IS NULL OR e.name LIKE CONCAT('%', :#{#searchRequest.name}, '%'))
+        AND (:#{#searchRequest.dobFrom} IS NULL OR e.dob >= :#{#searchRequest.dobFrom})
+        AND (:#{#searchRequest.dobTo} IS NULL OR e.dob <= :#{#searchRequest.dobTo})
+        AND (:#{#searchRequest.gender} IS NULL OR e.gender = :#{#searchRequest.gender})
+        AND (:#{#searchRequest.phone} IS NULL OR e.phone LIKE CONCAT('%', :#{#searchRequest.phone}, '%'))
+        AND (:#{#searchRequest.departmentId} IS NULL OR d.id = :#{#searchRequest.departmentId})
+        AND (
+        CASE
+        WHEN :#{#searchRequest.salaryRange} = '1-5' THEN e.salary < 5000000
+        WHEN :#{#searchRequest.salaryRange} = '5-10' THEN e.salary BETWEEN 5000000 AND 10000000
+        WHEN :#{#searchRequest.salaryRange} = '10-20' THEN e.salary BETWEEN 10000000 AND 20000000
+        WHEN :#{#searchRequest.salaryRange} = 'gt20' THEN e.salary > 20000000
+        ELSE TRUE
+        END
+        )
+        """, nativeQuery = true)
     List<Employee> findByAttributes(@Param("searchRequest") EmployeeSearchRequest searchRequest);
 
     List<Employee> findByNameContaining(String name);
